b466d592cdc714f670f27d72f928af9d9b45c9ed
Juantuporaqui <juantuporaqui@yahoo.es>
2026-01-27 17:09:07 +0100

    Delete chaladita directory

--- chaladita/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { BrowserRouter } from "react-router-dom";
-
-<BrowserRouter basename={import.meta.env.BASE_URL}>
-  <App />
-</BrowserRouter>
--- chaladita/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { HomePage } from './HomePage';
-export { PicassentPage } from './PicassentPage';
-export { QuartPage } from './QuartPage';
-export { MislataPage } from './MislataPage';
-export { OtrosPage } from './OtrosPage';
